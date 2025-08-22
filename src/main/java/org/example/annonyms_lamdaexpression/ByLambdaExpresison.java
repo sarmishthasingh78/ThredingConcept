6001ae6c4b3207f710bf17bd7164e4a73ab2be01
Sarmishtha Singh <sarmishtha78@gmail.com>
2025-08-22 18:18:22 +0530

    For a Testing

--- src/main/java/org/example/annonyms_lamdaexpression/ByLambdaExpresison.java
+++ src/main/java/org/example/annonyms_lamdaexpression/ByLambdaExpresison.java
@@ -4,7 +4,7 @@ public class ByLambdaExpresison {
     public static void main(String args[]){
         // Yha apn ne alg se run() ko override nhi kiya hai direct ho gya
         Runnable runnable = () ->{
-            System.out.println("Created Thread by Lambda expression");
+            System.out.println("Thread created in FEATURE branch using Lambda");
         };
         Thread thread = new Thread(runnable);
         thread.start();
